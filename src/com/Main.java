e883a9a2fcba0fbb6486dfc8ffcd723b98646b95
amatokus8669 <amatokus8669@users.noreply.github.com>
2014-04-20 16:01:26 +0200

    Update Main.java

--- src/com/Main.java
+++ src/com/Main.java
@@ -9,37 +9,39 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin{
-	
+
 	  private static final Logger log = Logger.getLogger("Minecraft");
 	  public static Economy econ = null;
-	
-	  
-	  
+	  private boolean ecoworks = false;
+
+
+
 	  @Override  
 	public void onEnable(){
-		
+
 		if (!setupEconomy() ) {
-			log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-			getServer().getPluginManager().disablePlugin(this);
+			log.severe(String.format("[%s] - Economy Disabled due to no Vault dependency found!", getDescription().getName()));
 			return;
-			}
-		
-		
+		}else{
+			ecoworks = true
+		}
+
+
 		this.getServer().getPluginManager().registerEvents(new Events(), this);
 		getLogger().info("Enabling the BetterAchievement Plugin");
 		getLogger().info("Check bukkit DevPage for updates!");
 	}
-	
-	  
-	  
+
+
+
 	  @Override
 	public void onDisable(){
-		  
+
         log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
         	
 	}  
-	  
-	  
+
+
 	//Vault economy soft-depend   
 	private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
@@ -52,5 +54,5 @@ public class Main extends JavaPlugin{
         econ = rsp.getProvider();
         return econ != null;
     }
-	
+
 }
