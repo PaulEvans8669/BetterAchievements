80801d355177298357cea8959f49e9c48d347e1f
amatokus8669 <amatokus8669@users.noreply.github.com>
2014-04-20 18:52:17 +0200

    Update Events.java

--- src/com/Events.java
+++ src/com/Events.java
@@ -10,14 +10,23 @@ import org.bukkit.event.player.PlayerAchievementAwardedEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+
+
 public class Events implements Listener{
 
+	public Main plugin;
 	
+	public Events(Main plugin){
+		this.plugin = plugin;
+		}	
+
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerAchievementAwarded(PlayerAchievementAwardedEvent AwEv){
-
+		
 		Location location = AwEv.getPlayer().getLocation();
 		String name = AwEv.getPlayer().getName();
+		
+		
 		AwEv.getPlayer().sendMessage(ChatColor.AQUA+""+ChatColor.BOLD+"Congratulations"+ChatColor.GOLD+" "+name+ChatColor.AQUA+"!"+ChatColor.BOLD+" "+"You earned an achievement!"+ChatColor.WHITE);
 		AwEv.getPlayer().playSound(location, Sound.LEVEL_UP, 10.0F, 0);
 		AwEv.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2400, 1));
