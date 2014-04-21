5229a2a29f09c320ff2a990feb076727f2726035
amatokus8669 <amatokus8669@users.noreply.github.com>
2014-04-21 13:14:56 +0200

    Update Main.java
    Added (null) Becasue I was having an error wit hthe Events class.
    
    Error between:  
    
    Main.class:
    		this.getServer().getPluginManager().registerEvents(new Events(), this);
    
    
    and
    
    Events.class:
    	public Main plugin;
    	
    	public Events(Main plugin){
    		this.plugin = plugin;
    		}

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,10 +2,7 @@ package com.gmail.amatokus8669.plugin.betterachievements;
 
 
 import java.util.logging.Logger;
-
 import net.milkbowl.vault.economy.Economy;
-
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +12,7 @@ public class Main extends JavaPlugin{
 
 	  private static final Logger log = Logger.getLogger("Minecraft");
 	  public static Economy econ = null;
+	  private boolean ecoworks = false;
 
 
 
@@ -22,10 +20,11 @@ public class Main extends JavaPlugin{
 	public void onEnable(){
 
 		if (!setupEconomy() ) {
-			log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-			getServer().getPluginManager().disablePlugin(this);
+			log.severe(String.format("[%s] - Economy Disabled due to no Vault dependency found!", getDescription().getName()));
 			return;
-			}
+		}else{
+			ecoworks = true;
+		}
 
 
 		this.getServer().getPluginManager().registerEvents(new Events(null), this);
